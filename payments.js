cc9a9284cf026b72625d5b6a498d280f3ec9c5da
firstcloudflash <firstcloudflash@gmail.com>
2024-08-10 12:16:44 +0100

    Create payments.js

---
+++ payments.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const router = express.Router();
+
+router.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'eBook Title',
+            images: ['https://example.com/image.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'https://example.com/success',
+    cancel_url: 'https://example.com/cancel',
+  });
+
+  res.json({ id: session.id });
+});
+
+module.exports = router;
